1970f8c7585910be6642772eb60d3529a5b825d6
Superchicken962 <58424347+Superchicken962@users.noreply.github.com>
2021-03-16 15:34:02 +1030

    Add files via upload

--- getdate.js
+++ getdate.js
@@ -2,9 +2,3 @@ var tdate = new Date();
 var date = tdate.getDate()+'/'+(tdate.getMonth()+1)+'/'+tdate.getFullYear();
 document.getElementById("showdate").innerHTML = date;
 var theday = tdate.getDate();
-
-var theday2 = "27";
-var duedate = "18";
-var daysuntildue = theday - duedate;
-var daysuntilduefinal = daysuntildue - daysuntildue - daysuntildue;
-document.getElementById("showduecountdown").innerHTML = daysuntilduefinal;
