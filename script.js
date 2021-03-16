4efe4e392fafe7f36df406fa96e9350fc0f6f686
Superchicken962 <58424347+Superchicken962@users.noreply.github.com>
2021-03-16 22:38:20 +1030

    update

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function speaknitrogencycle() {
     msg.volume = 1; // From 0 to 1
     msg.rate = 1; // From 0.1 to 10
     msg.pitch = 2; // From 0 to 2
-    msg.text = "The nitrogen cycle is the cycle of which nitrogen is converted into different and multiple chemicals as it circulates in the atmosphere. The process doesn’t end and loops back for whichever happens. Now, for the explanation of the process for the nitrogen cycle and the processes. Let us start with the animal, living our life until we die. Luckily, we don’t go to waste and we are decomposed and then turned into ammonium (NH4). How exciting, we then proceed to nitrification and we become nitrites (NO2), then nitrates (NO3). There are two paths we go from here; one is that we go into plants which can die and decompose or get eaten by an animal and have the process start again. The second path is more interesting, we become atmospheric nitrogen (N2). From the atmosphere we go back into the soil and can either live with the legume roots or we can go back into ammonification and start all again.";
+    msg.text = "The nitrogen cycle is the cycle of which nitrogen is converted into different and multiple chemicals as the nitrogen circulates in the atmosphere. The process doesn’t end and loops back for whichever happens. Now, for the explanation of the process for the nitrogen cycle and the processes. Let us start with the animal, living life until death. In the case the animal does not go to waste, the animal is decomposed and then turned into ammonium (NH4). How exciting, the animal will then proceed to nitrification and become nitrites (NO2), then nitrates (NO3). There are two paths the nitrates takes from here; one is that nitrates go into plants which can die and decompose or get eaten by an animal and have the process start again. The second path is more interesting, the nitrates become atmospheric nitrogen (N2). the atmospheric nitrogen goes back into the soil and can either live with the legume roots or go back into ammonification and start all again.";
     window.speechSynthesis.speak(msg);
 }
 
@@ -30,7 +30,7 @@ function speakcarboncycle() {
     msg.volume = 1; // From 0 to 1
     msg.rate = 1; // From 0.1 to 10
     msg.pitch = 2; // From 0 to 2
-    msg.text = "The carbon cycle is a biogeochemical cycle where carbon is traded among the earth’s different spheres; the atmosphere, the hydrosphere, biosphere, pedosphere, and the geosphere. the atmosphere is the layer that contains gasses, or air, if you will. These gasses are kept in place by earth’s gravity. The hydrosphere is the total water amount that is on the earth, including underground water and not just lakes and rivers. The biosphere pretty much represents and is the sum of all the ecosystems on the earth. It can also be known as the ecosphere. Pedosphere is simple, the most outer layer of the earth, composed of soils. Finally, we get to geosphere. The geosphere defines as the rocks, minerals, landforms of the surface and in the crust of the earth. Time to look at the carbon cycle. Now, we will start in the animal. Again. The animal will respire, releasing carbon. The animal will also release carbon after dying and decomposing. When respiring, the carbon is released into the atmosphere, where it becomes carbon dioxide (CO2). The carbon released from the decomposed animal also gets released into the atmosphere and becomes carbon dioxide (CO2). The decomposed animal will also compound into inorganic carbon which becomes organic carbon, which feeds into the plants. The cycle repeats now, the animal will either eat parts of the plant, or will breathe in oxygen produced by the plant.";
+    msg.text = "The carbon cycle is a biogeochemical cycle where carbon is traded among the earth’s different spheres; the atmosphere, the hydrosphere, biosphere, pedosphere, and the geosphere. the atmosphere is the layer that contains gasses, or air. These gasses are kept in place by earth’s gravity. The hydrosphere is the total water amount that is on the earth, including underground water and not just lakes and rivers. The biosphere pretty much represents and is the sum of all the ecosystems on the earth. It can also be known as the ecosphere. Pedosphere is simple, the most outer layer of the earth, composed of soils. Finally, we get to geosphere. The geosphere defines as the rocks, minerals, landforms of the surface and in the crust of the earth. Time to look at the carbon cycle. Now, we will start in the animal. Again. The animal will respire, releasing carbon. The animal will also release carbon after dying and decomposing. When respiring, the carbon is released into the atmosphere, where it becomes carbon dioxide (CO2). The carbon released from the decomposed animal also gets released into the atmosphere and becomes carbon dioxide (CO2). The decomposed animal will also compound into inorganic carbon which becomes organic carbon, which feeds into the plants. The cycle repeats now, the animal will either eat parts of the plant, or will breathe in oxygen produced by the plant.";
     window.speechSynthesis.speak(msg);
 }
 
@@ -66,6 +66,30 @@ function speakplantdietcarbon() {
     msg.volume = 1; // From 0 to 1
     msg.rate = 1; // From 0.1 to 10
     msg.pitch = 2; // From 0 to 2
-    msg.text = "The carbon cycle is simple, animals’ breath in oxygen and feed on the plants, they then breathe out carbon dioxide, which is taken in by the plants. When an animal or plant dies, it is decomposed and released as carbon dioxide. You could think of it as an infinite cycle. But then there were humans. Humans mostly eat meat, or animals, if you will. Some have “plant-based diets.” Now, according to (n/a, 2021); researchers found that the diets of people who eat more than 3.5 ounces of meat per day— about the size of a deck of playing cards—generate 15.8 pounds of carbon-dioxide equivalent (CO2e) each day, whereas vegetarians and vegans are responsible for 8.4 pounds and 6.4 pounds of CO2e, respectively. That’s almost half of the amount of CO2e generated when on a non-carnivorous diet, compared to the 7.4 pounds more that is made when eating meat. Too much carbon dioxide in the atmosphere will cause more heat to be trapped in the earth, like a greenhouse. Plant-based diets can be beneficial and will be beneficial, but the carbon cycle cannot recycle if there are no plants to photosynthesize. This will not likely happen as long as we are planting new plants when we take the old ones.";
+    msg.text = "The carbon cycle is simple, animals’ breath in oxygen and feed on the plants, they then breathe out carbon dioxide, which is taken in by the plants. When an animal or plant dies, it is decomposed and released as carbon dioxide. You could think of it as an infinite cycle. But then there were humans. Humans mostly eat meat, or animals. Some have “plant-based diets.” Now, according to (n/a, 2021); researchers found that the diets of people who eat more than 3.5 ounces of meat per day— about the size of a deck of playing cards—generate 15.8 pounds of carbon-dioxide equivalent (CO2e) each day, whereas vegetarians and vegans are responsible for 8.4 pounds and 6.4 pounds of CO2e, respectively. That’s almost half of the amount of CO2e generated when on a non-carnivorous diet, compared to the 7.4 pounds more that is made when eating meat. Too much carbon dioxide in the atmosphere will cause more heat to be trapped in the earth, like a greenhouse. Plant-based diets can be beneficial and will be beneficial, but the carbon cycle cannot recycle if there are no plants to photosynthesize. This will not likely happen as long as we are planting new plants when we take the old ones.";
+    window.speechSynthesis.speak(msg);
+}
+
+function speakplantdietnitrogen() {
+    var msg = new SpeechSynthesisUtterance();
+    msg.lang = 'en';
+    var voices = window.speechSynthesis.getVoices();
+    msg.voice = voices[10]; 
+    msg.volume = 1; // From 0 to 1
+    msg.rate = 1; // From 0.1 to 10
+    msg.pitch = 2; // From 0 to 2
+    msg.text = "In the nitrogen cycle, plants absorb nitrates from the soil to get their nitrogen. This is called assimilation. The nitrogen will be turned back into ammonium once the animal or plant has died and been decomposed. If the plants are taken for plant-based diets then not only will the plant not be able to be turned back into ammonium, it may also affect the animals as they also eat the plants. If there are limited plants, then there will be more animals going for the same plant and causing other animals to possibly die and releasing more nitrogen and causing the cycle to become unbalanced.";
+    window.speechSynthesis.speak(msg);
+}
+
+function speakbiodiversity() {
+    var msg = new SpeechSynthesisUtterance();
+    msg.lang = 'en';
+    var voices = window.speechSynthesis.getVoices();
+    msg.voice = voices[10]; 
+    msg.volume = 1; // From 0 to 1
+    msg.rate = 1; // From 0.1 to 10
+    msg.pitch = 2; // From 0 to 2
+    msg.text = "Biodiversity is the diverse variety of life in the ecosystem and on earth. For the ecosystems to work, there needs to be biodiversity. Different animals rely on other animal’s doings and so on. by taking plants for the plant-based diet, the biodiversity can be disrupted and can interrupt processes of animals with that plant in the ecosystem. Disrupting one process will lead to other processes being disrupted and the whole biodiversity ecosystem will have been impacted just for food for a plant-based diet.";
     window.speechSynthesis.speak(msg);
 }
