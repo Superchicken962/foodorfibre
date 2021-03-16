d05aba9573ef61a6735f66ab885369c6a41887fd
Superchicken962 <58424347+Superchicken962@users.noreply.github.com>
2021-03-16 21:07:16 +1030

    update

--- script.js
+++ script.js
@@ -30,6 +30,18 @@ function speakcarboncycle() {
     msg.volume = 1; // From 0 to 1
     msg.rate = 1; // From 0.1 to 10
     msg.pitch = 2; // From 0 to 2
-    msg.text = "";
+    msg.text = "The carbon cycle is a biogeochemical cycle where carbon is traded among the earth’s different spheres; the atmosphere, the hydrosphere, biosphere, pedosphere, and the geosphere. the atmosphere is the layer that contains gasses, or air, if you will. These gasses are kept in place by earth’s gravity. The hydrosphere is the total water amount that is on the earth, including underground water and not just lakes and rivers. The biosphere pretty much represents and is the sum of all the ecosystems on the earth. It can also be known as the ecosphere. Pedosphere is simple, the most outer layer of the earth, composed of soils. Finally, we get to geosphere. The geosphere defines as the rocks, minerals, landforms of the surface and in the crust of the earth. Time to look at the carbon cycle. Now, we will start in the animal. Again. The animal will respire, releasing carbon. The animal will also release carbon after dying and decomposing. When respiring, the carbon is released into the atmosphere, where it becomes carbon dioxide (CO2). The carbon released from the decomposed animal also gets released into the atmosphere and becomes carbon dioxide (CO2). The decomposed animal will also compound into inorganic carbon which becomes organic carbon, which feeds into the plants. The cycle repeats now, the animal will either eat parts of the plant, or will breathe in oxygen produced by the plant.";
+    window.speechSynthesis.speak(msg);
+}
+
+function speakextension() {
+    var msg = new SpeechSynthesisUtterance();
+    msg.lang = 'en';
+    var voices = window.speechSynthesis.getVoices();
+    msg.voice = voices[10]; 
+    msg.volume = 1; // From 0 to 1
+    msg.rate = 1; // From 0.1 to 10
+    msg.pitch = 2; // From 0 to 2
+    msg.text = "A plant-based diet for the human population could be possible. There are some people who are already on full plant-based diets, which is fine but them forcing this upon other people may influence the public in the other direction and to them not taking on a semi plant diet. We do not have to rule out meat altogether, meat consumption just needs to be controlled and limited. A start to this could be advertising how the cycles work and educating the population. Easing into this will take a fair while but in the end, emissions will be reduced.";
     window.speechSynthesis.speak(msg);
 }
