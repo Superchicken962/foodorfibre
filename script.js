579264b51085f873131cb7417ac37ece3a1ab2bd
Superchicken962 <58424347+Superchicken962@users.noreply.github.com>
2021-03-22 12:45:08 +1030

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function speakhome() {
     msg.voice = voices[10]; 
     msg.volume = 1; // From 0 to 1
     msg.rate = 1; // From 0.1 to 10
-    msg.pitch = 2; // From 0 to 2
+    msg.pitch = 1; // From 0 to 2
     msg.text = "A plant-based diet is a diet primarily focused on consumption of plants such as nuts, seeds, grains, beans etc. A Plant based diet does not automatically mean vegan or vegetarian. Plant-Based diets will most likely include meat, but may have them consumed once or twice week instead of regularly. The consumption of meat is dependant on the consumer.";
     window.speechSynthesis.speak(msg);
 }
@@ -29,7 +29,7 @@ function speakcarboncycle() {
     msg.voice = voices[10]; 
     msg.volume = 1; // From 0 to 1
     msg.rate = 1; // From 0.1 to 10
-    msg.pitch = 2; // From 0 to 2
+    msg.pitch = 1; // From 0 to 2
     msg.text = "The carbon cycle is a biogeochemical cycle where carbon is traded among the earth’s different spheres; the atmosphere, the hydrosphere, biosphere, pedosphere, and the geosphere. the atmosphere is the layer that contains gasses, or air. These gasses are kept in place by earth’s gravity. The hydrosphere is the total water amount that is on the earth, including underground water and not just lakes and rivers. The biosphere pretty much represents and is the sum of all the ecosystems on the earth. It can also be known as the ecosphere. Pedosphere is simple, the most outer layer of the earth, composed of soils. Finally, we get to geosphere. The geosphere defines as the rocks, minerals, landforms of the surface and in the crust of the earth. Time to look at the carbon cycle. Now, we will start in the animal. Again. The animal will respire, releasing carbon. The animal will also release carbon after dying and decomposing. When respiring, the carbon is released into the atmosphere, where it becomes carbon dioxide (CO2). The carbon released from the decomposed animal also gets released into the atmosphere and becomes carbon dioxide (CO2). The decomposed animal will also compound into inorganic carbon which becomes organic carbon, which feeds into the plants. The cycle repeats now, the animal will either eat parts of the plant, or will breathe in oxygen produced by the plant.";
     window.speechSynthesis.speak(msg);
 }
@@ -41,7 +41,7 @@ function speakextension() {
     msg.voice = voices[10]; 
     msg.volume = 1; // From 0 to 1
     msg.rate = 1; // From 0.1 to 10
-    msg.pitch = 2; // From 0 to 2
+    msg.pitch = 1; // From 0 to 2
     msg.text = "A plant-based diet for the human population could be possible. There are some people who are already on full plant-based diets, which is fine but them forcing this upon other people may influence the public in the other direction and to them not taking on a semi plant diet. We do not have to rule out meat altogether, meat consumption just needs to be controlled and limited. A start to this could be advertising how the cycles work and educating the population. Easing into this will take a fair while but in the end, emissions will be reduced.";
     window.speechSynthesis.speak(msg);
 }
@@ -53,7 +53,7 @@ function speakclimatechange() {
     msg.voice = voices[10]; 
     msg.volume = 1; // From 0 to 1
     msg.rate = 1; // From 0.1 to 10
-    msg.pitch = 2; // From 0 to 2
+    msg.pitch = 1; // From 0 to 2
     msg.text = "Refer to figure 3, this is the agricultural cycle. As observed in the image, you can see that Nitrous oxide, methane and carbon dioxide are released into the atmosphere from different ways. As you can see, fire will release methane, carbon dioxide and nitrous oxide. Methane can also be released from animals when they belch. Manure can release methane and nitrous oxide or be used as fertilizer which will eventually release carbon dioxide by soil respiration. According to europeanscientists.com, a shift to plant-based diets and limiting meat consumption by 60 grams per each person could reduce the agricultural emissions by up to 8 gigatons of carbon, yearly.";
     window.speechSynthesis.speak(msg);
 }
@@ -65,7 +65,7 @@ function speakplantdietcarbon() {
     msg.voice = voices[10]; 
     msg.volume = 1; // From 0 to 1
     msg.rate = 1; // From 0.1 to 10
-    msg.pitch = 2; // From 0 to 2
+    msg.pitch = 1; // From 0 to 2
     msg.text = "The carbon cycle is simple, animals’ breath in oxygen and feed on the plants, they then breathe out carbon dioxide, which is taken in by the plants. When an animal or plant dies, it is decomposed and released as carbon dioxide. You could think of it as an infinite cycle. But then there were humans. Humans mostly eat meat, or animals. Some have “plant-based diets.” Now, according to (n/a, 2021); researchers found that the diets of people who eat more than 3.5 ounces of meat per day— about the size of a deck of playing cards—generate 15.8 pounds of carbon-dioxide equivalent (CO2e) each day, whereas vegetarians and vegans are responsible for 8.4 pounds and 6.4 pounds of CO2e, respectively. That’s almost half of the amount of CO2e generated when on a non-carnivorous diet, compared to the 7.4 pounds more that is made when eating meat. Too much carbon dioxide in the atmosphere will cause more heat to be trapped in the earth, like a greenhouse. Plant-based diets can be beneficial and will be beneficial, but the carbon cycle cannot recycle if there are no plants to photosynthesize. This will not likely happen as long as we are planting new plants when we take the old ones.";
     window.speechSynthesis.speak(msg);
 }
@@ -77,7 +77,7 @@ function speakplantdietnitrogen() {
     msg.voice = voices[10]; 
     msg.volume = 1; // From 0 to 1
     msg.rate = 1; // From 0.1 to 10
-    msg.pitch = 2; // From 0 to 2
+    msg.pitch = 1; // From 0 to 2
     msg.text = "In the nitrogen cycle, plants absorb nitrates from the soil to get their nitrogen. This is called assimilation. The nitrogen will be turned back into ammonium once the animal or plant has died and been decomposed. If the plants are taken for plant-based diets then not only will the plant not be able to be turned back into ammonium, it may also affect the animals as they also eat the plants. If there are limited plants, then there will be more animals going for the same plant and causing other animals to possibly die and releasing more nitrogen and causing the cycle to become unbalanced.";
     window.speechSynthesis.speak(msg);
 }
@@ -89,7 +89,7 @@ function speakbiodiversity() {
     msg.voice = voices[10]; 
     msg.volume = 1; // From 0 to 1
     msg.rate = 1; // From 0.1 to 10
-    msg.pitch = 2; // From 0 to 2
+    msg.pitch = 1; // From 0 to 2
     msg.text = "Biodiversity is the diverse variety of life in the ecosystem and on earth. For the ecosystems to work, there needs to be biodiversity. Different animals rely on other animal’s doings and so on. by taking plants for the plant-based diet, the biodiversity can be disrupted and can interrupt processes of animals with that plant in the ecosystem. Disrupting one process will lead to other processes being disrupted and the whole biodiversity ecosystem will have been impacted just for food for a plant-based diet.";
     window.speechSynthesis.speak(msg);
 }
